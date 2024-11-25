341ff8cc06712232a12bc12137e21715f70aa8c8
Student <donyahajhassan@yahoo.com>
2024-11-25 22:23:26 +0330

    donya_validaition

--- user_validator.py
+++ user_validator.py
@@ -20,15 +20,16 @@ def user_validator(user):
          return False
 
 def password_validator(password):
-            if re.math(r"^[\w]{2,15}$", password):
+            if re.math(r"^[a-zA-Z\s\d]{2,15}$", password):
                 return True
             else:
                 return False
 
 
+#
+# def is_active_validator(is_active):
+#     return bool(re.match(r"^[a-zA-Z\s]{2,30}$", id))
 
-def is_active_validator(is_active):
-    return bool(re.match(r"^[a-zA-Z\s]{2,30}$", id))
 
-def age_validator(age):
-    return  0 < age< 150
+
+
